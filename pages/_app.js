669d436e0b97f7dfe5a6853e0a24765146f58f1a
Shae Allison <sallison@adkgroup.com>
2022-03-10 19:11:37 -0500

    adding reset and colocating js

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,33 @@
-import { globalStyles } from '../shared/styles'
+import emotionReset from 'emotion-reset';
+import { css, Global} from '@emotion/react'
+
+const globalStyles = (
+  <Global
+    styles={css`
+      ${emotionReset}
+
+      * {
+        box-sizing: border-box;
+      }
+
+      html {
+        font-size: 62.5% !important;
+        scroll-behavior: smooth;
+      }
+
+      body {
+        font-family: 'Barlow', sans-serif;
+        font-weight: 400;
+        font-size: 1.6rem;
+      }
+
+      a {
+        color: inherit;
+        text-decoration: none;
+      }
+    `}
+  />
+)
 
 const App = ({ Component, pageProps }) => (
   <>
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,54 @@
-import { Animated, Basic, bounce, Combined } from '../shared/styles'
+import { css, keyframes } from '@emotion/react'
+import styled from '@emotion/styled'
+
+const basicStyles = css`
+  background-color: white;
+  color: cornflowerblue;
+  border: 1px solid lightgreen;
+  border-right: none;
+  border-bottom: none;
+  box-shadow: 5px 5px 0 0 lightgreen, 10px 10px 0 0 lightyellow;
+  transition: all 0.1s linear;
+  margin: 3rem 0;
+  padding: 1rem 0.5rem;
+`
+
+const hoverStyles = css`
+  &:hover {
+    color: white;
+    background-color: lightgray;
+    border-color: aqua;
+    box-shadow: -15px -15px 0 0 aqua, -30px -30px 0 0 cornflowerblue;
+  }
+`
+const bounce = keyframes`
+  from {
+    transform: scale(1.01);
+  }
+  to {
+    transform: scale(0.99);
+  }
+`
+
+const Basic = styled.div`
+  ${basicStyles};
+`
+
+const Combined = styled.div`
+  ${basicStyles};
+  ${hoverStyles};
+  & code {
+    background-color: linen;
+  }
+`
+const Animated = styled.div`
+  ${basicStyles};
+  ${hoverStyles};
+  & code {
+    background-color: linen;
+  }
+  animation: ${({ animation }) => animation} 0.2s infinite ease-in-out alternate;
+`
 
 const Home = () => (
   <div>
